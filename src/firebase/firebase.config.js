fba41ce96180fbd7436f14764e3a8f0496a41789
himaloyash <himaloyashis98@gmail.com>
2023-05-12 13:46:07 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDr2Wx3h8fesin7nvwP8VwCTxOhIb29Cyc",
+  authDomain: "car-doctor-client-practice.firebaseapp.com",
+  projectId: "car-doctor-client-practice",
+  storageBucket: "car-doctor-client-practice.appspot.com",
+  messagingSenderId: "48630608079",
+  appId: "1:48630608079:web:323d5368b695e703d787ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -1,35 +1,47 @@
 import React from 'react';
+import logo from '../../assets/images/login/login.svg'
 
 const Login = () => {
+
+    const handleSubmit =(event)=>{
+        event.preventDefault()
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+        console.log(email, password)
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-                <div className="text-center lg:text-left w-1/2">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+                <div className=" max-auto w-1/2">
 
+                    <img src={logo} alt="" />
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
-                        <h1 className="text-5xl font-bold">Login</h1>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl ml-6 bg-base-100">
+                    <form onSubmit={handleSubmit}>
+                        <div className="card-body">
+                            <h1 className="text-5xl font-bold">Login</h1>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
                         </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
